44e7a7bac9aca87b158f47c2e1c364018f491c8d
Jhonanthan Campos <dionathancampos@hotmail.com>
2023-11-21 23:00:14 -0300

    Camada de serviço, regitro de componentes

---
+++ src/main/java/com/cannysters/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.cannysters.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cannysters.course.entities.User;
+import com.cannysters.course.repositories.userRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private userRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
