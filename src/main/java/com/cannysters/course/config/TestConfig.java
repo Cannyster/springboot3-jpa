88b24ed3c0ea8f30a53ed7a57399029a08e4a131
Jhonanthan Campos <dionathancampos@hotmail.com>
2023-12-01 14:03:00 -0300

    Payment associação um-para-um

--- src/main/java/com/cannysters/course/config/TestConfig.java
+++ src/main/java/com/cannysters/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cannysters.course.entities.Category;
 import com.cannysters.course.entities.Order;
 import com.cannysters.course.entities.OrderItem;
+import com.cannysters.course.entities.Payment;
 import com.cannysters.course.entities.Product;
 import com.cannysters.course.entities.User;
 import com.cannysters.course.entities.enums.OrderStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
